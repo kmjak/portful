b21dd98fc65ca883f50f1eec743f5c1bb653a3e7
kmjak <tymkoron1221@icloud.com>
2025-05-12 01:59:23 +0900

    refactor: 再利用しやすいようにclassNameとpropsを受け取るようにした

--- frontend/src/components/ui/PageTitle.tsx
+++ frontend/src/components/ui/PageTitle.tsx
@@ -1,12 +1,19 @@
-import { JSX, ReactNode } from "react";
+import cn from "@/utils/tailwindcss/cn";
+import { HTMLProps, JSX, ReactNode } from "react";
 
-interface PageTitleProps {
+interface PageTitleProps extends HTMLProps<HTMLHeadingElement> {
   children: ReactNode;
 }
 
-export default function PageTitle({ children }: PageTitleProps): JSX.Element {
+export default function PageTitle({ children, className, ...props }: PageTitleProps): JSX.Element {
   return (
-    <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-2 md:mb-3 lg:mb-4">
+    <h1
+      className={cn(
+        "text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-2 md:mb-3 lg:mb-4",
+        className
+      )}
+      {...props}
+    >
       {children}
     </h1>
   );
