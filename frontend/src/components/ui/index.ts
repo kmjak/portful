ed4f2959e03b2482b967f33550ba069d5d08a377
kmjak <tymkoron1221@icloud.com>
2025-05-12 02:06:47 +0900

    feat: 標準的なテキスト用のコンポーネントを作成し、index.tsに追加した

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as Input } from "./Input";
 export { default as Label } from "./Label";
 export { default as PageTitle } from "./PageTitle";
+export { default as Text } from "./Text";
