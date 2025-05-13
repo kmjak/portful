7d5e0f6f4911fad1e85ce02c0fad6025c973fd05
kmjak <tymkoron1221@icloud.com>
2025-05-14 03:12:15 +0900

    feat: 新規登録とログインのどちらでも使うコンポーネントをindex.tsに追加した

---
+++ frontend/src/components/auth/shared/index.ts
@@ -0,0 +1,3 @@
+export { default as AuthButtons } from "./AuthButtons";
+export { default as LoginProviderButtons } from "./LoginProviderButtons";
+export { default as AuthInputField } from "./AuthInputField";
