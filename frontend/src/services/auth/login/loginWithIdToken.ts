6de101f12ed70c5d1df03eda56ee6b2f56f2889d
kmjak <tymkoron1221@icloud.com>
2025-05-14 03:03:06 +0900

    feat: idTokenを使ってAPI router経由でbackendで認証を行う関数

---
+++ frontend/src/services/auth/login/loginWithIdToken.ts
@@ -0,0 +1,40 @@
+import { hostUrl } from "@/config/host";
+
+interface LoginWithIdTokenProps {
+  readonly idToken: string;
+}
+
+/**
+ * @description
+ * IDTokenを使用してバックエンドで認証を行う関数
+ *
+ * @param {LoginWithIdTokenProps} props - IDトークンを含むオブジェクト
+ * @param {string} props.idToken - IDトークン
+ *
+ * @returns {Promise<boolean>} - ログイン成功時はtrue、失敗時はfalseを返す
+ */
+export default async function loginWithIdToken({
+  idToken,
+}: LoginWithIdTokenProps): Promise<boolean> {
+  try {
+    const response: Response = await fetch(`${hostUrl}/api/v1/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${idToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to login with ID token");
+    }
+
+    return true;
+  } catch (error) {
+    if (process.env.NODE_ENV === "development") {
+      console.error("Error signing in with Firebase:", error);
+    }
+    alert("ログイン中にエラーが発生しました。");
+    return false;
+  }
+}
