cb94bd5f800aa4973f6fa0a598917000d6a9ff71
kmjak <tymkoron1221@icloud.com>
2025-05-14 15:04:39 +0900

    chore: serviceAccountを別ファイルに切り出して別ファイルから読み込む方式に改善した

--- backend/lib/firebase/firebase.js
+++ backend/lib/firebase/firebase.js
@@ -1,15 +1,10 @@
 import admin from "firebase-admin";
 import { cert } from "firebase-admin/app";
-
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-};
+import { serviceAccount } from "../../config/firebase/serviceAccount.js";
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: cert(serviceAccount),
   });
 }
-export default admin;
+export { admin };
