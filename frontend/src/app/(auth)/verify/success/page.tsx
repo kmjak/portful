0e498736bf0292214ca4e05a56836cacfb708462
kmjak <tymkoron1221@icloud.com>
2025-05-28 02:51:24 +0900

    refactor: successページのコンポーネント名の修正とsuccessとfailureのコンポーネントにJSDocを追加

--- frontend/src/app/(auth)/verify/success/page.tsx
+++ frontend/src/app/(auth)/verify/success/page.tsx
@@ -2,7 +2,15 @@ import { PageTitle, Text } from "@/components/ui";
 import Link from "next/link";
 import { JSX } from "react";
 
-export default function Failure(): JSX.Element {
+/**
+ * @description
+ * メールアドレス認証失敗ページ
+ * ユーザーがメールアドレスの認証に成功した場合に表示されるページです。
+ * ユーザーにログインページへのリンクを提供します。
+ *
+ * @returns {JSX.Element} メールアドレス認証成功ページのコンポーネント
+ */
+export default function Success(): JSX.Element {
   return (
     <div className="flex flex-col items-center justify-center mt-5 md:mt-8 lg:mt-12">
       <PageTitle className="text-center">メールアドレス認証成功</PageTitle>
