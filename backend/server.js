4c0dce49e67b35c4569aaf21dd36b24dbf476366
kmjak <tymkoron1221@icloud.com>
2025-05-28 02:37:01 +0900

    feat: app.useに/authのパスはauthRouterと設定した

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("Server is running...");
+import { PORT } from "./config/env/port.js";
+import "./lib/env/dotenv.js";
+import { app, server } from "./lib/express/expressServer.js";
+import authRouter from "./routes/auth/auth.js";
+
+app.use("/auth", authRouter);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
