745b71df572d4a163dad5a65a0628fccd30f60bd
kmjak <tymkoron1221@icloud.com>
2025-06-06 08:49:24 +0900

    feat: sessionのlastAccessedを更新する処理を追加した

---
+++ backend/models/session/updateSessionWithToken.js
@@ -0,0 +1,22 @@
+import { prismaClient } from "../../lib/prisma/client.js";
+
+/**
+ * @description
+ * sessionのlastAccessedを更新する関数
+ *
+ * @param sessionToken - セッショントークン
+ * @param currentDate - 現在の日付
+ * @return {Promise<Object>} 更新されたセッションのデータ
+ */
+export default async function updateSessionWithToken(sessionToken, currentDate) {
+  const session = await prismaClient.session.update({
+    where: {
+      sessionToken,
+    },
+    data: {
+      lastAccessed: currentDate,
+    },
+  });
+
+  return session;
+}
