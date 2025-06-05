bf17b881af3bb609c7a384e36c7e93ac93f86848
kmjak <tymkoron1221@icloud.com>
2025-06-06 08:54:35 +0900

    feat: FirebaseのユーザーIDを使用して、ユーザーデータを取得または新規作成する処理を追加した

---
+++ backend/services/user/getOrCreateUser.js
@@ -0,0 +1,19 @@
+import createUser from "../../models/user/createUser.js";
+import getUserWithFid from "../../models/user/getUserWithFid.js";
+
+/**
+ * @description
+ * FirebaseのユーザーIDを使用して、ユーザーデータを取得する関数
+ * もしユーザーデータが存在しない場合は、新しくユーザーを作成する
+ *
+ * @param firebaseUserId - FirebaseのユーザーID
+ * @returns {Promise<Object>} ユーザーデータ
+ */
+export default async function getOrCreateUser(firebaseUserId) {
+  let userData = await getUserWithFid(firebaseUserId);
+  if (userData === null) {
+    userData = await createUser(firebaseUserId);
+  }
+
+  return userData;
+}
