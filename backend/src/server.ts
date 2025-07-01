7329855b7fd64a92c7c9e782bd01c9375605e0c2
kmjak <tymkoron1221@icloud.com>
2025-07-01 22:41:59 +0900

    move: server.tsをsrc配下においた

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import { PORT } from "../config/env/port.js";
+import "../lib/env/dotenv.js";
+import { app, server } from "../lib/express/expressServer.js";
+import authRouter from "../routes/auth/auth.js";
+
+app.use("/auth", authRouter);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
