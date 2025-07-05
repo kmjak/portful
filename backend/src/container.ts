6ec53fc49e2bf70dd068092382ac03d0c399f7ad
kmjak <tymkoron1221@icloud.com>
2025-07-05 16:50:58 +0900

    feat: DIコンテナにEnvHostConfigProviderをHostConfigProviderとして登録

--- backend/src/container.ts
+++ backend/src/container.ts
@@ -1,1 +1,9 @@
 import "reflect-metadata";
+import { container } from "tsyringe";
+import { HostConfigProvider } from "@/domain/provider/host/HostConfigProvider";
+import { EnvHostConfigProvider } from "@/infrastructure/provider/host/EnvHostConfigProvider";
+
+// Provider
+container.register<HostConfigProvider>("HostConfigProvider", {
+  useClass: EnvHostConfigProvider,
+});
