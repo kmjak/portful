3121cd7063ace83ed0f2f4fbeed072b5532024a6
kmjak <tymkoron1221@icloud.com>
2025-07-05 16:45:11 +0900

    docs: JSDocを追加した

--- backend/src/usecase/middleware/ApiKeyAuthUseCase.ts
+++ backend/src/usecase/middleware/ApiKeyAuthUseCase.ts
@@ -2,6 +2,10 @@ import { injectable, inject } from "tsyringe";
 import { HostConfigProvider } from "@/domain/provider/host/HostConfigProvider";
 import { ApiKey } from "@/domain/model/host";
 
+/**
+ * @class ApiKeyAuthUseCase
+ * @description APIキーの認証を行うユースケース
+ */
 @injectable()
 export class ApiKeyAuthUseCase {
   constructor(
@@ -9,7 +13,12 @@ export class ApiKeyAuthUseCase {
     private readonly hostConfigProvider: HostConfigProvider
   ) {}
 
-  public async execute(apiKey: string): Promise<boolean> {
+  /**
+   * @description APIキーを検証するメソッド
+   * @param {string} apiKey - 検証するAPIキー
+   * @returns {ApiKey} APIキーが有効な場合はtrue、無効な場合はfalseを返す
+   */
+  public execute(apiKey: string): boolean {
     const hostConfig = this.hostConfigProvider.load();
     const envApiKey = hostConfig.getApiKey();
 
