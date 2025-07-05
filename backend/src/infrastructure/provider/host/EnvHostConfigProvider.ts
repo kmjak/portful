cea9f770ec4bceb86a5884ced6d2598c057d746b
kmjak <tymkoron1221@icloud.com>
2025-07-05 16:42:00 +0900

    docs: JSDocを追加した

--- backend/src/infrastructure/provider/host/EnvHostConfigProvider.ts
+++ backend/src/infrastructure/provider/host/EnvHostConfigProvider.ts
@@ -1,7 +1,16 @@
 import { ApiKey, FrontendUrl, HostConfig } from "@/domain/model/host";
 import { HostConfigProvider } from "@/domain/provider/host/HostConfigProvider";
 
+/**
+ * @class EnvHostConfigProvider
+ * @description 環境変数からホストの設定を提供するクラス
+ */
 export class EnvHostConfigProvider implements HostConfigProvider {
+  /**
+   * @description 環境変数からホストの設定を読み込み、HostConfigを返す。
+   * @returns {HostConfig} HostConfig - ホストの設定
+   * @throws {Error} - 環境変数が設定されていない場合
+   */
   public load(): HostConfig {
     const frontendUrl = FrontendUrl.of(process.env.FRONTEND_URL || "");
     const apiKey = ApiKey.of(process.env.HOST_X_API_KEY || "");
