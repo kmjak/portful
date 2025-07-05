f50d2b6d746b2e3f1b9efed0e7f6684bc3e372bf
kmjak <tymkoron1221@icloud.com>
2025-07-05 16:58:28 +0900

    feat: DIコンテナに環境変数をロードする処理を追加した

--- backend/src/container.ts
+++ backend/src/container.ts
@@ -2,6 +2,9 @@ import "reflect-metadata";
 import { container } from "tsyringe";
 import { HostConfigProvider } from "@/domain/provider/host/HostConfigProvider";
 import { EnvHostConfigProvider } from "@/infrastructure/provider/host/EnvHostConfigProvider";
+import { EnvLoader } from "./interface/config/envLoader";
+
+EnvLoader.load();
 
 // Provider
 container.register<HostConfigProvider>("HostConfigProvider", {
