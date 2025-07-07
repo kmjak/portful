d255cf935cd3eebb1458d421abe36f80e9e3f85a
kmjak <tymkoron1221@icloud.com>
2025-07-08 01:23:46 +0900

    refactor: ApiKeyAuthUseCaseにエラーハンドリングを追加した

--- backend/src/usecase/middleware/ApiKeyAuthUseCase.ts
+++ backend/src/usecase/middleware/ApiKeyAuthUseCase.ts
@@ -1,6 +1,6 @@
-import { injectable, inject } from "tsyringe";
-import { HostConfigProvider } from "@/domain/provider/host/HostConfigProvider";
 import { ApiKey } from "@/domain/model/host";
+import { HostConfigProvider } from "@/domain/provider/host/HostConfigProvider";
+import { inject, injectable } from "tsyringe";
 
 /**
  * @class ApiKeyAuthUseCase
@@ -17,11 +17,19 @@ export class ApiKeyAuthUseCase {
    * @description APIキーを検証するメソッド
    * @param {string} apiKey - 検証するAPIキー
    * @returns {ApiKey} APIキーが有効な場合はtrue、無効な場合はfalseを返す
+   * @throws {Error} - APIキーの検証に失敗した場合にエラーをスローする
    */
   public execute(apiKey: string): boolean {
-    const hostConfig = this.hostConfigProvider.load();
-    const envApiKey = hostConfig.getApiKey();
+    try {
+      const hostConfig = this.hostConfigProvider.load();
+      const envApiKey = hostConfig.getApiKey();
 
-    return envApiKey.equals(ApiKey.of(apiKey));
+      return envApiKey.equals(ApiKey.of(apiKey));
+    } catch (error) {
+      throw new Error(`
+        [ApiKeyAuthUseCase] APIキーの検証に失敗しました。
+        ${error}
+      `);
+    }
   }
 }
