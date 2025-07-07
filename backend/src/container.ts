176b1bce66e80647e4614a694528a9859f96c750
kmjak <tymkoron1221@icloud.com>
2025-07-08 00:52:28 +0900

    feat: DIコンテナにEnvFirebaseConfigProverをFirebaseConfigProviderとして登録した

--- backend/src/container.ts
+++ backend/src/container.ts
@@ -1,7 +1,9 @@
-import "reflect-metadata";
-import { container } from "tsyringe";
 import { HostConfigProvider } from "@/domain/provider/host/HostConfigProvider";
 import { EnvHostConfigProvider } from "@/infrastructure/provider/host/EnvHostConfigProvider";
+import "reflect-metadata";
+import { container } from "tsyringe";
+import { FirebaseConfigProvider } from "./domain/provider/firebase/FirebaseConfigProvider";
+import { EnvFirebaseConfig } from "./infrastructure/provider/firebase/EnvFirebaseConfig";
 import { EnvLoader } from "./interface/config/EnvLoader";
 
 EnvLoader.load();
@@ -9,4 +11,8 @@ EnvLoader.load();
 // Provider
 container.register<HostConfigProvider>("HostConfigProvider", {
   useClass: EnvHostConfigProvider,
+});
+
+container.register<FirebaseConfigProvider>("FirebaseConfigProvider", {
+  useClass: EnvFirebaseConfig,
 });
