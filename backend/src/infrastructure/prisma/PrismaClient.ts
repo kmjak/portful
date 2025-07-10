b0d847ce433e6ef00b9d15c7889fbff1ecfa9c7b
kmjak <tymkoron1221@icloud.com>
2025-07-11 01:36:09 +0900

    rename: PrismaAdminよりもPrismaClientの方がしっくりきたので修正した

--- backend/src/infrastructure/prisma/PrismaClient.ts
+++ backend/src/infrastructure/prisma/PrismaClient.ts
@@ -1,26 +1,26 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient as PrismaClientLib } from "@prisma/client";
 import { injectable, singleton } from "tsyringe";
 
 /**
- * @class PrismaAdmin
+ * @class PrismaClient
  * @description Prismaクライアントをシングルトンで提供するクラス
  */
 @singleton()
 @injectable()
-export class PrismaAdmin {
-  private readonly prisma: PrismaClient;
+export class PrismaClient {
+  private readonly prisma: PrismaClientLib;
 
   public constructor() {
-    this.prisma = new PrismaClient({
+    this.prisma = new PrismaClientLib({
       log: ["query", "warn", "error"],
     });
   }
 
   /**
    * @description Prismaクライアントのインスタンスを返す
-   * @returns {PrismaClient}
+   * @returns {PrismaClientLib}
    */
-  public getAdmin(): PrismaClient {
+  public getClient(): PrismaClientLib {
     return this.prisma;
   }
 }
