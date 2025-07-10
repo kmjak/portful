b372577a6839801faba574c4ce8de50279ae69c1
kmjak <tymkoron1221@icloud.com>
2025-07-11 00:45:30 +0900

    refactor: UserIdをFirebaseUserIdに変更したことに伴う修正を行なった

--- backend/src/infrastructure/firebase/FirebaseAuthService.ts
+++ backend/src/infrastructure/firebase/FirebaseAuthService.ts
@@ -1,4 +1,4 @@
-import { UserId } from "@/domain/model/user";
+import { FirebaseUserId } from "@/domain/model/user";
 import { FirebaseAdmin } from "@/infrastructure/firebase/FirebaseAdmin";
 import { inject, injectable } from "tsyringe";
 
@@ -15,16 +15,16 @@ export class FirebaseAuthService {
   constructor(@inject(FirebaseAdmin) private admin: FirebaseAdmin) {}
 
   /**
-   * @description FirebaseのIDトークンを検証し、ユーザーIDを取得するメソッド
+   * @description FirebaseのIDトークンを検証し、Firebase User IDを取得するメソッド
    * @param {string} idToken - FirebaseのIDトークン
-   * @returns {Promise<UserId>} - 検証されたユーザーIDを返す
+   * @returns {Promise<FirebaseUserId>} - 検証されたFirebase User IDを返す
    * @throws {Error} - IDトークンの検証に失敗した場合にエラーをスローする
    */
-  public async verifyIdToken(idToken: string): Promise<UserId> {
+  public async verifyIdToken(idToken: string): Promise<FirebaseUserId> {
     try {
       const app = this.admin.getApp();
       const { user_id: firebaseUserId } = await app.auth().verifyIdToken(idToken);
-      return UserId.of(firebaseUserId);
+      return FirebaseUserId.of(firebaseUserId);
     } catch (error) {
       throw new Error(`
         [FirebaseAuthService] FirebaseのIDトークンの検証に失敗しました。
