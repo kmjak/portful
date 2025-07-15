64ce58a6f108f57e94f03c8e595bd0be22bc2e0d
kmjak <tymkoron1221@icloud.com>
2025-07-15 13:03:47 +0900

    feat: UserのEntityやValue Objectをindexに追加した

--- backend/src/domain/model/user/index.ts
+++ backend/src/domain/model/user/index.ts
@@ -1,1 +1,5 @@
+export { CreatedAt } from "./CreatedAt.js";
 export { FirebaseUserId } from "./FirebaseUserId.js";
+export { User } from "./User.js";
+export { UserId } from "./UserId.js";
+export { UserName } from "./UserName.js";
